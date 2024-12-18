58f25c533d3b752bedaaeaad1a76f2601df1f521
Ankit Gope <ankitgope21@gmail.com>
2024-12-18 20:55:36 +0530

    added order model

---
+++ backend/src/models/Order.model.js
@@ -0,0 +1,42 @@
+import mongoose,{Schema} from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required:true
+    },
+    products: [
+        {
+            product: {
+                type:mongoose.Schema.Types.ObjectId,
+                ref : "Product",
+                required:true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    totalPrice: {
+        type: Number,
+        required: true
+    },  
+    paymentMethod: {
+        type: String,
+        required: true
+    },
+    orderStatus: {
+        type: String,
+        enum: [ "processing", "shipped", "delivered"],
+        default: "processing"
+    },
+    shippingAddress: {
+        type: String,
+        required: true
+    },
+},
+     {timestamps:true});
+
+export const Order = mongoose.model("Order", orderSchema);
