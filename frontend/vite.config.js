58fbc2171e2df5fd6157bf8b53c1a8924816953d
Ankit Gope <ankitgope21@gmail.com>
2024-12-27 13:01:58 +0530

    Update vite.config.js done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,6 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: "https://ecommerce-app-backend-blgm.onrender.com", 
-        changeOrigin: true,
-        secure: false,
       }
     }
   },  
