26b91c494c3172049dde764385fad6125500e255
Ankit Gope <ankitgope21@gmail.com>
2024-12-27 09:51:00 +0530

    link

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: `http://localhost:8000`, 
+        target: `https://ecommerce-app-86uo.onrender.com`, 
         changeOrigin: true,
         secure: false,
       }
