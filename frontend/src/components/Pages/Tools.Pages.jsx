d02a5bdcc212d5d9840544ec5fe16886d68551b4
Ankit Gope <ankitgope21@gmail.com>
2024-12-27 18:59:30 +0530

    Update Tools.Pages.jsx

--- frontend/src/components/Pages/Tools.Pages.jsx
+++ frontend/src/components/Pages/Tools.Pages.jsx
@@ -9,7 +9,7 @@ const Tools_Pages = () => {
     useEffect(() => {
         try {
             const fetchProducts = async () => {
-                const response = await axios.get(`/api/products/getProductsByCategory/Tools`)
+                const response = await axios.get(`https://ecommerce-app-backend-blgm.onrender.com/api/products/getProductsByCategory/Tools`)
                 // console.log(response.data.message)
                 // console.log(Array.isArray(response.data.message))
                 setProducts(response.data.message)
