d80919c261ee0145defce37a5843a22428679990
Ankit Gope <ankitgope21@gmail.com>
2025-01-19 19:08:30 +0530

    Update Header.Buyer.jsx

--- frontend/src/components/Header/Header.Buyer.jsx
+++ frontend/src/components/Header/Header.Buyer.jsx
@@ -9,7 +9,7 @@ function B_Header() {
   const Logout = async () => {
     const userConfirm = confirm("Are you sure you want to logout?");
     if (userConfirm) {
-      const response = await axios.post("/api/users/logoutUser", {
+      const response = await axios.post("https://ecommerce-app-backend-kyd3.onrender.com/api/users/logoutUser", {
         withCredentials: true,
       });
       if (response.status === 200) {
