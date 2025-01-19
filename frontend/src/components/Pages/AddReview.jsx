4a2d7f69ea64f2fdae10af2a46da084ab7168b47
Ankit Gope <ankitgope21@gmail.com>
2025-01-19 19:04:22 +0530

    Update AddReview.jsx

--- frontend/src/components/Pages/AddReview.jsx
+++ frontend/src/components/Pages/AddReview.jsx
@@ -27,7 +27,7 @@ const AddReview = () => {
     
         try {
             const response = await axios.post(
-                `https://ecommerce-app-backend-blgm.onrender.com/api/reviews/addReview/${product}`, 
+                `https://ecommerce-app-backend-kyd3.onrender.com/api/reviews/addReview/${product}`, 
                 { rating: formData.rating, comment: formData.comment },
                 { withCredentials: true }
             );
