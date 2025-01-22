75179535402a0c407220001650ee2365ecaae0aa
Ankit Gope <ankitgope21@gmail.com>
2025-01-22 21:40:14 +0530

    Update cloudinary.js

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,7 +1,35 @@
-import { v2 as cloudinary } from "cloudinary"
-import fs from "fs"
+// import { v2 as cloudinary } from "cloudinary"
+// import fs from "fs"
 
 
+
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// const uploadOnCloudinary = async (localFilePath)=>
+//     {
+//         try {
+//             if(!localFilePath) return null;
+//             const response = await cloudinary.uploader.upload(localFilePath,{
+//                 resource_type:"auto"
+//             })
+//             // file has been uploaded successfully
+//             // console.log('file is uploaded on cloudinary',response.url);
+//             fs.unlinkSync(localFilePath)
+//             return response
+//         } catch (error) {
+//             fs.unlinkSync(localFilePath) /*removes the locally saved temporary file
+//             as the upload operation got failed*/
+//             return null;
+//         }
+//     }
+
+// export {uploadOnCloudinary};
+
+import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -9,22 +37,21 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary = async (localFilePath)=>
-    {
-        try {
-            if(!localFilePath) return null;
-            const response = await cloudinary.uploader.upload(localFilePath,{
-                resource_type:"auto"
-            })
-            // file has been uploaded successfully
-            // console.log('file is uploaded on cloudinary',response.url);
-            fs.unlinkSync(localFilePath)
-            return response
-        } catch (error) {
-            fs.unlinkSync(localFilePath) /*removes the locally saved temporary file
-            as the upload operation got failed*/
-            return null;
-        }
+const uploadOnCloudinary = async (buffer) => {
+    try {
+        if (!buffer) return null;
+        
+        const response = await cloudinary.uploader.upload_stream({
+            resource_type: "auto"
+        }, (error, result) => {
+            if (error) return null;
+            return result;
+        }).end(buffer);
+        
+        return response;
+    } catch (error) {
+        return null;
     }
+}
 
-export {uploadOnCloudinary};
+export { uploadOnCloudinary };
